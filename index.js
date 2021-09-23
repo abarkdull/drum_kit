65e51660952684f2378eff505f2bfa892b93c54a
abarkdull <abarkdull@gmail.com>
2021-09-23 14:51:36 -0600

    added keystroke event listener

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 var drum_buttons = document.querySelectorAll(".drum");
 
 drum_buttons.forEach(drum_button => drum_button.addEventListener("click", function () {
-     alert('i got clicked')}));
+        
+        switch(this.innerHTML) {
+            case 'w':
+                (new Audio('sounds/crash.mp3')).play();
+                break;
+            case 'a':
+                (new Audio('sounds/kick-bass.mp3')).play();
+                break;
+            case 's':
+                (new Audio('sounds/snare.mp3 ')).play();
+                break;
+            case 'd':
+                (new Audio('sounds/tom-1.mp3')).play();
+                break;
+            case 'j':
+                (new Audio('sounds/tom-2.mp3')).play();
+                break;
+            case 'k':
+                (new Audio('sounds/tom-3.mp3')).play();
+                break;
+            case 'l':
+                (new Audio('sounds/tom-4.mp3')).play();
+                break;
+        }
+
+    }    
+));
+
+document.addEventListener('keydown', function (event) {
+
+    switch (event.key) {
+
+        case 'w':
+            (new Audio('sounds/crash.mp3')).play();
+            break;
+        case 'a':
+            (new Audio('sounds/kick-bass.mp3')).play();
+            break;
+        case 's':
+            (new Audio('sounds/snare.mp3 ')).play();
+            break;
+        case 'd':
+            (new Audio('sounds/tom-1.mp3')).play();
+            break;
+        case 'j':
+            (new Audio('sounds/tom-2.mp3')).play();
+            break;
+        case 'k':
+            (new Audio('sounds/tom-3.mp3')).play();
+            break;
+        case 'l':
+            (new Audio('sounds/tom-4.mp3')).play();
+            break;
+    }
+})
